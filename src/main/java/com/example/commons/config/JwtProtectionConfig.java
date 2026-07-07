d1f6901b5d380884e36dd69a296c0a098a94375f
sarwadnya <sarwadnya1996@gmail.com>
2026-07-07 21:17:15 +0530

    added interceptors

--- src/main/java/com/example/commons/config/JwtProtectionConfig.java
+++ src/main/java/com/example/commons/config/JwtProtectionConfig.java
@@ -1,12 +1,27 @@
 package com.example.commons.config;
 
+import com.example.commons.intercetor.JwtProtectionInterceptor;
+import com.example.commons.validaator.JwtValidator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class JwtProtectionConfig implements WebMvcConfigurer {
 
-
-
+    @Bean
+    public JwtValidator validator(){
+        return new JwtValidator();
+    }
+    @Bean
+    public JwtProtectionInterceptor interceptor(JwtValidator validator){
+        return new JwtProtectionInterceptor(validator);
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(interceptor(validator()))
+                .addPathPatterns("/api/**");
+    }
 }
--- src/main/java/com/example/commons/intercetor/JwtProtectionInterceptor.java
+++ src/main/java/com/example/commons/intercetor/JwtProtectionInterceptor.java
@@ -1,15 +1,52 @@
 package com.example.commons.intercetor;
 
+import com.example.commons.annotation.Authenticate;
+import com.example.commons.validaator.JwtValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
 
 public class JwtProtectionInterceptor implements HandlerInterceptor {
 
-    @Override
-    public boolean  preHandle(HttpServletRequest request,
-                              HttpServletResponse response,
-                              Object handler){
+    public final JwtValidator validator;
+    public final ObjectMapper objectMapper = new ObjectMapper();
 
+    public JwtProtectionInterceptor(JwtValidator validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler)  throws Exception{
+        if (!(handler instanceof HandlerMethod handlerMethod)) {
+            return true;
+        }
+        boolean isProtected = handlerMethod.hasMethodAnnotation(Authenticate.class) ||
+                handlerMethod.getBeanType().isAnnotationPresent(Authenticate.class);
+
+        if (!isProtected) {
+            return true;
+        }
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Beared ")) {
+            writeError(response, HttpServletResponse.SC_UNAUTHORIZED, "Missing or Malformed JWT");
+            return false;
+        }
+        String token = authHeader.substring(7);
+        validator.validateJwtToken(token);
+        return true;
+    }
+
+    private void writeError(HttpServletResponse response, int scUnauthorized, String missingOrMalformedJwt) throws Exception {
+        response.setStatus(scUnauthorized);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), Map.of("error", missingOrMalformedJwt));
     }
 }
+
